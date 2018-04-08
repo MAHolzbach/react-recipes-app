71b6cf0f308f067961c536bdbb86c263f92f6036
Mike Holzbach <maholzbach@gmail.com>
2018-04-08 11:32:27 -0400

    Set up queries to get recipes from AWS Appsync

---
+++ src/appsync.js
@@ -0,0 +1,7 @@
+export default {
+  graphqlEndpoint:
+    "https://zitjo3o7pfftzhp5gis375xp2y.appsync-api.us-east-1.amazonaws.com/graphql",
+  region: "us-east-1",
+  authenticationType: "API_KEY",
+  apiKey: "da2-ozkyve4aqrag7jnmdsuuffxfda"
+};

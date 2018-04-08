0d60eb4d472234f68aad21912859ae9d16f6043f
Mike Holzbach <maholzbach@gmail.com>
2018-04-08 12:28:15 -0400

    Added add recipe logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import uuidV4 from "uuid/v4";
 
 import { compose, graphql } from "react-apollo";
 import ListRecipes from "./queries/ListRecipes";
+import CreateRecipe from "./mutations/CreateRecipe";
 
 class App extends Component {
+  state = {
+    name: "",
+    ingredient: "",
+    direction: "",
+    ingredients: [],
+    directions: []
+  };
+
+  onChange = (key, value) => {
+    this.setState({ [key]: value });
+  };
+
+  addIngredient = () => {
+    if ((this.state.ingredient = "")) return;
+
+    const ingredients = this.state.ingredients;
+
+    ingredients.push(this.state.ingredient);
+
+    this.setState({
+      ingredient: ""
+    });
+  };
+
+  addDirection = () => {
+    if ((this.state.direction = "")) return;
+
+    const directions = this.state.directions;
+
+    directions.push(this.state.direction);
+
+    this.setState({
+      direction: ""
+    });
+  };
+
+  addRecipe = () => {
+    const { name, ingredients, directions } = this.state;
+    this.props.onAdd({
+      name,
+      ingredients,
+      directions,
+      id: uuidV4()
+    });
+    this.setState({
+      name: "",
+      ingredient: "",
+      direction: ""
+    });
+  };
+
   render() {
     console.log("props: ", this.props);
     return (
@@ -15,7 +68,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         {this.props.recipes.map((recipe, index) => (
-          <div>
+          <div key={index}>
             <p>{recipe.name}</p>
           </div>
         ))}
@@ -24,11 +77,23 @@ class App extends Component {
   }
 }
 
-export default graphql(ListRecipes, {
-  options: {
-    fetchPolicy: "cache-and-network"
-  },
-  props: props => ({
-    recipes: props.data.listRecipes ? props.data.listRecipes.items : []
+const syles = {};
+
+export default compose(
+  graphql(ListRecipes, {
+    options: {
+      fetchPolicy: "cache-and-network"
+    },
+    props: props => ({
+      recipes: props.data.listRecipes ? props.data.listRecipes.items : []
+    })
+  }),
+  graphql(CreateRecipe, {
+    props: props => ({
+      onAdd: recipe =>
+        props.mutate({
+          variable: recipe
+        })
+    })
   })
-})(App);
+)(App);
